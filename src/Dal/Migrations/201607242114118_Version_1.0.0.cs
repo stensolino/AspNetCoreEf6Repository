3beccc930dba90b43de15b05a503c2c2732e59b6
Adnan Mulalic <stensolino@gmail.com>
2016-07-24 23:15:38 +0200

    Enable Migrations

---
+++ src/Dal/Migrations/201607242114118_Version_1.0.0.cs
@@ -0,0 +1,26 @@
+namespace Dal.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Version_100 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
