4f10041f31f39f66b02d2fdf73ceb634a5e9503b
Adnan Mulalic <stensolino@gmail.com>
2016-07-24 23:22:22 +0200

    Add Repository

---
+++ src/Dal/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Dal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    interface IRepository
+    {
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(int id);
+        User SaveUser(User user);
+        User UpdateUser(User user);
+        bool DeleteUser(int id);
+    }
+}
---
+++ src/Dal/Repository.cs
@@ -0,0 +1,106 @@
+﻿using Dal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class Repository
+    {
+        #region Constructor
+
+        private ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+        #region Users
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            try
+            {
+                var users = _context.Users.ToList();
+                return users;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public User GetUserById(int id)
+        {
+            try
+            {
+                var user = _context.Users.FirstOrDefault(u => u.Id == id);
+                return user;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public User SaveUser(User user)
+        {
+            try
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+
+                return user;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public User UpdateUser(User user)
+        {
+            try
+            {
+                var userDb = _context.Users.FirstOrDefault(u => u.Id == user.Id);
+                userDb = user;
+                _context.SaveChanges();
+
+                return user;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool DeleteUser(int id)
+        {
+            try
+            {
+                var user = _context.Users.FirstOrDefault(u => u.Id == id);
+
+                if (user == null)
+                {
+                    return false;
+                }
+
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+    }
+}
