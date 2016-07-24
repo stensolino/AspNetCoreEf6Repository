cb7a6d401fef9138a43c8aeb14c8b60a488cb688
Adnan Mulalic <stensolino@gmail.com>
2016-07-24 23:09:36 +0200

    Add ApplicationDbContext

---
+++ src/Dal/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base(@"Data Source=.\SQLEXPRESS;Initial Catalog=AspNetCoreEf6Repository;Integrated Security=True; MultipleActiveResultSets=True") { }
+    }
+}
