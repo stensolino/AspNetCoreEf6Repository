879037c41e135f7b39769c0f5a9f60da2fd4e056
Adnan Mulalic <stensolino@gmail.com>
2016-07-24 23:12:17 +0200

    Add User model

--- src/Dal/ApplicationDbContext.cs
+++ src/Dal/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dal.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -10,5 +11,7 @@ namespace Dal
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext() : base(@"Data Source=.\SQLEXPRESS;Initial Catalog=AspNetCoreEf6Repository;Integrated Security=True; MultipleActiveResultSets=True") { }
+
+        public DbSet<User> Users { get; set; }
     }
 }
